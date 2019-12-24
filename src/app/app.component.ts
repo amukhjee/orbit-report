33f9c78f83165a29196715d9353e1530acae7792
Supriya Mukherjee <supriyamukherjee@Supriyas-MBP.lan1>
2019-12-23 18:08:36 -0600

    save files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit  } from '@angular/core';
-import { Satellite } from './satellite'
+import { Satellite } from './satellite';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/satellite.spec.ts
+++ src/app/satellite.spec.ts
@@ -2,6 +2,6 @@ import { Satellite } from './satellite';
 
 describe('Satellite', () => {
   it('should create an instance', () => {
-    expect(new Satellite(name, type, launchDate, orbitType, operational)).toBeTruthy();
+    expect(new Satellite()).toBeTruthy();
   });
 });
