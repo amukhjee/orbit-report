fb9706caee93e1c04ab410c5a6e8fe14fe60d2e7
Supriya Mukherjee <supriyamukherjee@Supriyas-MBP.lan1>
2019-12-24 01:01:42 -0600

    save 8 searching done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,11 @@ import { Satellite } from './satellite'
 export class AppComponent implements OnInit  {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -29,11 +31,26 @@ export class AppComponent implements OnInit  {
          this.sourceList.push(satellite);
          
           }
- console.log(this.sourceList);
+ // make a copy of the sourceList to be shown to the user
+ this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  }
 
  ngOnInit() { }
+
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
